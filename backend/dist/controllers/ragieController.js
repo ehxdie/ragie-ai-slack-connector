13a7dda7657d615b049b366424550f97b3fd2a65
Edidiong <ephraimedy@gmail.com>
2024-12-06 18:52:03 +0100

    Compiled typescipt code to dist folder

---
+++ backend/dist/controllers/ragieController.js
@@ -0,0 +1,27 @@
+import { addQuery } from '../services/queryService.js';
+import { getAnswers } from '../services/answerService.js';
+import { ragieIntegration } from '../integrations/ragie.js';
+export const postQuery = async (req, res) => {
+    const query = req.query.paramName;
+    if (query) {
+        addQuery(query);
+        // Dynamically trigger the answer generation
+        console.log(`Processing query: ${query}`);
+        // Automatically reruns 
+        await ragieIntegration();
+        res.status(200).json({ msg: query });
+    }
+    else {
+        res.status(400).json({ error: 'Query parameter not provided' });
+    }
+};
+export const getResponse = async (req, res) => {
+    const answers = getAnswers();
+    if (answers) {
+        res.status(200).json({ response: answers });
+    }
+    else {
+        res.status(400).json({ error: 'No answers available' });
+    }
+};
+// module.exports = { postQuery, getResponse }
