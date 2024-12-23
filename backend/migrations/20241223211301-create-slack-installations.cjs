944e9f5e8037df7a6fd96b87a37e3fbc2c00c854
Edidiong <ephraimedy@gmail.com>
2024-12-23 22:42:40 +0100

    Created slack_installations table

---
+++ backend/migrations/20241223211301-create-slack-installations.cjs
@@ -0,0 +1,70 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('slack_installations', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      teamId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      teamName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      botUserId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      botAccessToken: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userAccessToken: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      appId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      scopes: {
+        type: Sequelize.JSONB,
+        allowNull: false,
+      },
+      enterpriseId: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      isEnterpriseInstall: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      timestamp: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('slack_installations');
+  },
+};
